3811b4ec48686ab915a97ea9d19fec0d96ab4a44
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2024-07-08 19:21:58 -0600

    new component

---
+++ frontend/src/components/NewComponent/NewComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class SampleComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Sample Component
+      </div>
+    );
+  }
+}
